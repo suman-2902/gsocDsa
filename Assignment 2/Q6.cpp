29b7020354cf1797d49c4721405017b9037c757f
suman-2902 <102145634+suman-2902@users.noreply.github.com>
2022-05-04 20:46:02 +0530

    Update Q6.cpp

--- Assignment 2/Q6.cpp
+++ Assignment 2/Q6.cpp
@@ -1,1 +1,39 @@
+         
+#include <bits/stdc++.h>
+#define MAX_SIZE 1000 
+
+int main()
+{
+    int arr[MAX_SIZE], size, i;
+    int max1, max2;
+
+    cout<<"Enter size of the array (1-1000): \n";
+    cin>>size;
+ 
+    cout<<"Enter elements in the array: \n";
+    for(i=0; i<size; i++)
+    {
+        cin>>arr[i];
+    }
+
+    max1 = max2 = INT_MIN;
+ 
+    for(i=0; i<size; i++)
+    {
+        if(arr[i] > max1)
+        {
+            max2 = max1;
+            max1 = arr[i];
+        }
+        else if(arr[i] > max2 && arr[i] < max1)
+        {
+            max2 = arr[i];
+        }
+    }
+
+    cout<<"First largest =\n"<<max1<<endl;
+    cout<<"Second largest =\n"<<max2;
+
+    return 0;
+}
 
