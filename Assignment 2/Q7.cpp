9a200d9c47f1ab1812a56e0d3e945418b263f085
suman-2902 <102145634+suman-2902@users.noreply.github.com>
2022-05-05 09:58:20 +0530

    Update Q7.cpp

--- Assignment 2/Q7.cpp
+++ Assignment 2/Q7.cpp
@@ -1,24 +1,26 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
 
   int i, n;
-  bool prime = true;  
-  cin >> n;
+  bool isprime=true;
   
+  cin >> n;
+
+
   if (n == 0 || n == 1) {
-    prime = false;
+    isprime = false;
   }
- 
-  for (i = 2; i <= n/2; ++i) {
+
+  for (i = 2; i <=sqrt(n); i++) {
     if (n % i == 0) {
-      prime = false;
+      isprime = false;
       break;
     }
   }
 
-  if (prime)
+  if (isprime)
     cout << n << " is a prime number";
   else
     cout << n << " is not a prime number";
