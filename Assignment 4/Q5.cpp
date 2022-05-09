d3bb3921c2577f2c2c10bd1fe8ee5f3d6c0a1541
suman-2902 <102145634+suman-2902@users.noreply.github.com>
2022-05-09 23:44:40 +0530

    Create Q5.cpp

---
+++ Assignment 4/Q5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+    string s2="";         
+        int len=s.length(); 
+        
+        for(int i=0;i<len;i++){
+            
+            if(isalpha(s[i])||isdigit(s[i])){
+            s[i]=tolower(s[i]);
+            s2+=s[i];
+            
+        }
+        }
+        
+        s=s2;
+        reverse(s2.begin(),s2.end());
+        
+        return s2==s;    
+    }
+};
