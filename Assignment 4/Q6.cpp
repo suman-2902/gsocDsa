2986bfb2440e702bb5cc82e0c6a4b36ca7ddae6c
suman-2902 <102145634+suman-2902@users.noreply.github.com>
2022-05-10 09:46:25 +0530

    Update Q6.cpp

--- Assignment 4/Q6.cpp
+++ Assignment 4/Q6.cpp
@@ -1,22 +1,28 @@
-class Solution {
+ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x < 0)
             return false;
         
 
-        long int num , rev = 0, rem = 0;
+        long int num=x , rev = 0, rem = 0;
         
 	
-        while(num != 0)
+        while(num!= 0)
         {
+            
             rem = num%10;
+            cout<<rem<<" ";
+            
             rev = rev*10 + rem;
-            num/= 10;
+            cout<<rev<<" ";
+            num=num/10;
+            cout<<num;
         }
         
+        cout<<x<<" "<<rev;
 	
-        if(num == rev)
+        if(x == rev)
             return true;  
         else
             return false;
