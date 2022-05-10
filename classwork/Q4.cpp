3b3b76863ec1d2d8614dfc2de08586901dfba21c
suman-2902 <102145634+suman-2902@users.noreply.github.com>
2022-05-10 09:44:19 +0530

    Create Q4.cpp

---
+++ classwork/Q4.cpp
@@ -0,0 +1,31 @@
+//pallindrome number
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        
+
+        long int num=x , rev = 0, rem = 0;
+        
+	
+        while(num!= 0)
+        {
+            
+            rem = num%10;
+            cout<<rem<<" ";
+            
+            rev = rev*10 + rem;
+            cout<<rev<<" ";
+            num=num/10;
+            cout<<num;
+        }
+        
+        cout<<x<<" "<<rev;
+	
+        if(x == rev)
+            return true;  
+        else
+            return false;
+    }
+};
