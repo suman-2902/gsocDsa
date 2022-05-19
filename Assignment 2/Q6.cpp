da7e834cd6a7d5c23aefaa4236a42378b9ff002d
Suman kumari <102145634+suman-2902@users.noreply.github.com>
2022-05-19 09:53:03 +0530

    Update Q6.cpp

--- Assignment 2/Q6.cpp
+++ Assignment 2/Q6.cpp
@@ -1,39 +1,43 @@
-         
-#include <bits/stdc++.h>
-#define MAX_SIZE 1000 
+  #include <bits/stdc++.h>
+using namespace std;
+int main(){
 
-int main()
-{
-    int arr[MAX_SIZE], size, i;
-    int max1, max2;
+int n;
+cin>>n;
+int temp;
+int max1;
+int max2;
+int arr[25];
 
-    cout<<"Enter size of the array (1-1000): \n";
-    cin>>size;
- 
-    cout<<"Enter elements in the array: \n";
-    for(i=0; i<size; i++)
-    {
-        cin>>arr[i];
-    }
-
-    max1 = max2 = INT_MIN;
- 
-    for(i=0; i<size; i++)
-    {
-        if(arr[i] > max1)
-        {
-            max2 = max1;
-            max1 = arr[i];
-        }
-        else if(arr[i] > max2 && arr[i] < max1)
-        {
-            max2 = arr[i];
-        }
-    }
-
-    cout<<"First largest =\n"<<max1<<endl;
-    cout<<"Second largest =\n"<<max2;
-
-    return 0;
+for(int i=0;i<n;i++){
+    
+  cin>>arr[i];
+    
 }
-
+max1=arr[0];
+max2=arr[1];
+if(max2>max1){
+    temp=max1;
+    max1=max2;
+    max2=temp;
+}
+for(int i=2;i<n;i++){
+    
+    if(arr[i]>=max1){
+        max2=max1;
+        max1=arr[i];
+    }
+    else if(max2==max1){
+        cout<<max1;
+    }
+    
+   else if(arr[i]>max2&&arr[i]!=max1){
+        max2=arr[i];
+    }
+}
+    cout<<max1<<" "<<max2;
+    return 0;
+ 
+    
+    
+}
