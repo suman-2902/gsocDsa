8b9391bafeb86e8f2be0d4c5c6770c9423c44f54
Suman kumari <102145634+suman-2902@users.noreply.github.com>
2022-06-02 22:21:53 +0530

    Create Q3.cpp

---
+++ Assignment 11/Q3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& ar) {
+        int n=ar.size(),i;
+        
+        if(n>=3){
+            
+            for( i=0;i<n-1;i++)
+                
+            {
+                return
+        distance(ar.begin(),max_element(ar.begin(),ar.end()));
+            }
+        }   
+        
+         return 0;
+        
+    }
+};
