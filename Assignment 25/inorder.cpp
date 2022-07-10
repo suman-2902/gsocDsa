5c63ad91807bd39dac83b4f7cb9986ae3a0b1a39
Suman kumari <102145634+suman-2902@users.noreply.github.com>
2022-07-10 22:16:20 +0530

    Create inorder.cpp

---
+++ Assignment 25/inorder.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int>res;
+    vector<int> inorderTraversal(TreeNode* root) {
+     if(root!=NULL){
+         inorderTraversal(root->left);
+         res.push_back(root->val);
+         inorderTraversal(root->right);
+     }  
+        return res;
+    }
+};
