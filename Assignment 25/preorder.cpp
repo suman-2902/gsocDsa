00df30e9b874ede471c860ba15291614e21e876e
Suman kumari <102145634+suman-2902@users.noreply.github.com>
2022-07-11 10:22:14 +0530

    Update preorder.cpp

--- Assignment 25/preorder.cpp
+++ Assignment 25/preorder.cpp
@@ -1,24 +1,13 @@
-class Solution {
+  class Solution {
 public:
+    vector<int>res;
     vector<int> preorderTraversal(TreeNode* root) {
-     if (!root)
-      return {};
-
-    vector<int> res;
-    stack<TreeNode*> stack{{root}};
-
-    while (!stack.empty()) {
-      root = stack.top(), stack.pop();
-      res.push_back(root->val);
-        
-      if (root->right)
-        stack.push(root->right);
-        
-      if (root->left)
-        stack.push(root->left);
-    }
-
-
-    return res;  
+     if(root!=NULL){
+         res.push_back(root->val);
+         preorderTraversal(root->left);
+         
+         preorderTraversal(root->right);
+     }  
+        return res;
     }
 };
