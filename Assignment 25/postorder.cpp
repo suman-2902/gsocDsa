1fd581fb6f38cad7942139bc266bae0276a272a7
Suman kumari <102145634+suman-2902@users.noreply.github.com>
2022-07-11 10:24:20 +0530

    Update postorder.cpp

--- Assignment 25/postorder.cpp
+++ Assignment 25/postorder.cpp
@@ -1,24 +1,15 @@
-class Solution {
+  class Solution {
 public:
+    vector<int>res;
     vector<int> postorderTraversal(TreeNode* root) {
-     if (!root)
-      return {};
-
-    vector<int> res;
-    stack<TreeNode*> stack{{root}};
-
-    while (!stack.empty()) {
-      root = stack.top(), stack.pop();
-      res.push_back(root->val);
+     if(root!=NULL){
         
-      if (root->left)
-        stack.push(root->left);
-        
-      if (root->right)
-        stack.push(root->right);
-    }
-
-    reverse(begin(res), end(res));
-    return res;  
+         postorderTraversal(root->left);
+         
+         postorderTraversal(root->right);
+         
+          res.push_back(root->val);
+     }  
+        return res;
     }
 };
