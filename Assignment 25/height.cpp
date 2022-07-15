499a0ecf1d72fe3770fef8ca2704308731c9a439
Suman kumari <102145634+suman-2902@users.noreply.github.com>
2022-07-15 10:39:43 +0530

    Update height.cpp

--- Assignment 25/height.cpp
+++ Assignment 25/height.cpp
@@ -9,3 +9,36 @@
     
         return 1+max(height(root->left),height(root->right));
     }
+
+
+
+
+//using queue
+   int height(Node* root) {
+        if(!root){
+            return 1;
+        }
+        queue<Node*>Q;
+        Q.push(root);
+        int level=0;
+        while(!Q.empty()){
+            int size=Q.size();
+            while(size--){
+                 Node*temp=Q.front();
+          // cout<<temp->data<<" ";
+           
+           if(temp->left){
+               Q.push(temp->left);
+            }
+            if(temp->right){
+                Q.push(temp->right);
+            }
+            Q.pop(); 
+        }
+        level++;
+        } 
+    
+    return level-1;
+    }
+
+};
