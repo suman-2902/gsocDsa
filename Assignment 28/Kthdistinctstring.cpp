367964e6b8782ae6463b255c28fd1ded3a7f22af
Suman kumari <102145634+suman-2902@users.noreply.github.com>
2022-07-20 09:18:58 +0530

    Update Kthdistinctstring.cpp

--- Assignment 28/Kthdistinctstring.cpp
+++ Assignment 28/Kthdistinctstring.cpp
@@ -1,11 +1,11 @@
-class Solution {
+ class Solution {
 public:
     string kthDistinct(vector<string>& arr, int k) {
         unordered_map<string, int>mp;
-        for (const string& target : arr){
+        for (string& target : arr){
             mp[target]++;
         }
-        for (const string& target : arr){
+        for (string& target : arr){
             if (mp[target] == 1 && --k == 0)
             {
                 return target;
