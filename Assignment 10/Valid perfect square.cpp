375642041dd23e265fc6cc921debad4a33db4ab5
Suman kumari <102145634+suman-2902@users.noreply.github.com>
2022-07-26 09:39:16 +0530

    Update Valid perfect square.cpp

--- Assignment 10/Valid perfect square.cpp
+++ Assignment 10/Valid perfect square.cpp
@@ -1,25 +1,28 @@
  class Solution {
 public:
-    typedef unsigned long long ULL;
-    bool isPerfectSquare(ULL num) {
-        ULL beg=1, end=num;
-        ULL sqrt=num;
-        while(beg<=end){
-            ULL mid=(beg+end)/2;
-            if(mid*mid==num){
-                return true;
-            }
-            else if(mid*mid>num){
-                end=mid-1;
-            }
-            else{
-                sqrt=mid;
-                beg=mid+1;
-            }
-        }
-        if(sqrt*sqrt==num){
+    bool isPerfectSquare(int num) {
+        long long int mid;
+        int high =num,low=0;
+        if(num==1)
             return true;
+        if(num==0)
+            return false;
+        while(low<high)
+        {
+            mid=(low+high)/2;
+            
+            if(mid*mid==num)
+                return true;
+            else if(mid*mid<num)
+            {  
+                low= mid+1;
+                
+            }
+            else if(mid*mid>num)
+            {
+                high = mid ;
+            }
         }
-        return false;     
+        return false;
     }
 };
