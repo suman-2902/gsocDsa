2cc320e6a7c38d6a5f0d69f61ff3581c3aed25a0
Suman kumari <102145634+suman-2902@users.noreply.github.com>
2022-07-26 09:48:27 +0530

    Update Find target indices after sorting array.cpp

--- Assignment 10/Find target indices after sorting array.cpp
+++ Assignment 10/Find target indices after sorting array.cpp
@@ -1,30 +1,36 @@
+ vector<int> count_sort(vector<int>&A)
+{
+     int max = *max_element(A.begin(), A.end());
+        int min = *min_element(A.begin(), A.end());
+        int range = max - min + 1;
+        int n = A.size();
+        vector<int> B(range), C(n);
+        for (int i = 0; i < n; i++)
+            B[A[i] - min]++;
+ 
+        for (int i = 1; i < range; i++)
+            B[i] += B[i - 1];
+ 
+        for (int i = n - 1; i >= 0; i--) {
+            C[B[A[i] - min] - 1] = A[i];
+            B[A[i] - min]--;
+        }
+        return C;
+}
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        int n=nums.size();
-        
-        int i,beg=0,end=n;
-        
-        vector<int> index;
-        
-        for(i=0;i<n-1;i++){
-            
-            for(int j=i;j<n;j++){
-                
-                if(nums[i]>nums[j]){
-                    
-                    swap(nums[i],nums[j]);
-                }
+        vector<int>B=count_sort(nums),ans;
+        for(int i=0;i<B.size();i++)
+        {
+            if(B[i]==target)
+            {
+                ans.push_back(i);
             }
+            if(B[i]>target)
+                break;
         }
-        for(int k=0;k<n;k++){
-            
-            if(nums[k]==target){
-                
-                index.push_back(k);
-            }
-        }
-        return index;
-        
+       
+        return ans;
     }
 };
